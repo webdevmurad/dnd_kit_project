144d71d6405b66793044153ec9fda4094268c6a1
m.arslangiraev <m.arslangiraev@iblschool.ru>
2023-11-14 16:52:27 +0300

    #5 Добавление задач для доски

---
+++ src/components/TaskCard.tsx
@@ -0,0 +1,20 @@
+import {Task} from "../types.ts";
+
+interface Props {
+  task: Task
+}
+function TaskCard({ task }: Props) {
+  return (
+    <div className="
+      bg-mainBackgroundColor p-2.5 h-[100px]
+      min-h-[100px] items-center flex text-left
+      rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500
+      cursor-grab
+      "
+    >
+      { task.content }
+    </div>
+  )
+}
+
+export default TaskCard;
